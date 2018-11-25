0b9ba133c72cd9a0bd6ad4bb643d3864c4232e5e
David Bourke <davidrbourke@gmail.com>
2018-11-25 21:38:37 +0000

    Small refactor

--- scorer.go
+++ scorer.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
 func main() {
@@ -10,53 +10,49 @@ func main() {
 }
 
 // Score receives game array and returns a score as int
-func Score(score []string) (total int) {
-
-	// var total int
-	for i := 0; i < len(score); i++ {
-		if i >= 10 {
-			return
-		}
-		val := score[i]
-
-		if len(val) == 2 && string(val[1]) == "/" {
+func Score(game []string) (total int) {
+	for i := 0; i < len(game) && i < 10; i++ {
+		frame := game[i]
+		if strings.Contains(frame, "/") {
 			var nxt int
-			if len(score)-(i+1) >= 1 {
-				nxt = scr(score[i+1][0])
+			if hasNextFrame(game, i) {
+				nxt = bowlToInt(game[i+1][0])
 			}
 			total += 10 + nxt
 			continue
 		}
-
-		if string(val[0]) == "X" {
-			total += getFrameScore(val, false)
-			rem := len(score) - (i + 1)
-			if rem >= 1 {
+		if string(frame[0]) == "X" {
+			total += getFrameScore(frame, false)
+			if hasNextFrame(game, i) {
 				var isBonus bool
 				if i+2 >= 10 {
 					isBonus = true
 				}
-				if score[i+1] == "X" {
-					total += getFrameScore(score[i+1], false)
-					total += getFrameScore(score[i+2], isBonus)
+				if game[i+1] == "X" {
+					total += getFrameScore(game[i+1], false)
+					total += getFrameScore(game[i+2], isBonus)
 				} else {
-					total += getFrameScore(score[i+1], false)
+					total += getFrameScore(game[i+1], false)
 				}
 			}
 			continue
 		}
-
-		total += getFrameScore(val, false)
+		total += getFrameScore(frame, false)
 	}
 	return
 }
 
-func scr(n byte) int {
+func bowlToInt(n byte) int {
 	if n == '-' {
 		return 0
 	}
-	val, _ := strconv.Atoi(string(n))
-	return val
+	// val, _ := strconv.Atoi(string(n))
+	// return val
+	return int(n - '0')
+}
+
+func hasNextFrame(game []string, currentFrame int) bool {
+	return len(game)-(currentFrame+1) >= 1
 }
 
 func getFrameScore(frame string, isBonus bool) int {
@@ -65,14 +61,14 @@ func getFrameScore(frame string, isBonus bool) int {
 		return 10
 	}
 
-	if len(frame) == 2 && frame[1] == '/' {
+	if strings.Contains(frame, "/") {
 		return 10
 	}
 
-	first := scr(frame[0])
+	first := bowlToInt(frame[0])
 	var second int
 	if len(frame) == 2 && !isBonus {
-		second = scr(frame[1])
+		second = bowlToInt(frame[1])
 	}
 	return first + second
 }
